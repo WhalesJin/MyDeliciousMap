9b34587982e277c65ab1e01ef635d8f797ef3768
Whales <Whales.w.Jin@gmail.com>
2023-10-09 14:36:02 +0900

    📝 Docs: 프로젝트 파일 및 gitignore 파일 생성
    Signed-off-by: Whales <Whales.w.Jin@gmail.com>

---
+++ YummyMap/YummyMap/YummyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummyMapApp.swift
+//  YummyMap
+//
+//  Created by Whales on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct YummyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
